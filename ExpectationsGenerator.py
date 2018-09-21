f38151bb8df8a04a2bc3d9c5ed69f9a56906e308
noahreifsnyder <noahreifsnyder@gmail.com>
2018-09-21 14:54:45 -0400

    decided to go with one graph build function that iterates from starting_state, WIP

--- ExpectationsGenerator.py
+++ ExpectationsGenerator.py
@@ -1,17 +1,19 @@
 #Takes in a graph from pyhop, performs necessary calculations to add expectations.
 import copy
 def genExpectations(policy,starting_state):
-    graph=buildGraph(policy)
+    graph=buildGraph(policy,starting_state)
     for member in graph:
         print(member,len(graph[member]))
     return
-def buildGraph(policy):
-    e="edges"
-    be="back_edges"
-    ce="cross_edges"
-    v="verticies"
+def buildGraph(policy,starting_state):#adds verticies and edges, and initialized graph components back and cross edges.
+    e = "edges"
+    be = "back_edges"
+    ce = "cross_edges"
+    v = "verticies"
     graph={e:set(),be:set(),ce:set(),v:set()}
-    for state in policy:
+    finished=False
+    state=starting_state
+    while not finished:
         action=policy[state]
         graph[e].add((state,action))
         for child in action.children:
