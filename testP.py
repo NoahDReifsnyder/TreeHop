091bf556f0c9f717e3f17b10a2f99ad5d532f38c
NoahDReifsnyder <noahreifsnyder@gmail.com>
2018-11-23 23:40:46 -0500

    Basic Tower Building Domain built

--- testP.py
+++ testP.py
@@ -4,70 +4,51 @@ import pyhop as treehop
 from testD import *
 from collections import defaultdict
 from random import *
+import collections
 from ExpectationsGenerator import gen_expectations
-
+import time
 
 
 state=treehop.State('state')
-state.above={}
-state.behind={}
-state.below={}
-state.infront={}
-state.lit={"B1":0,"B2":0,"B3":0}
-state.beacons={}
-state.agent={'Agent1':1}
+state.on={}
 state.clear={}
-state.fuel={'Agent1':10}
-n=5
-placed=[]
-for b in state.lit:
-    x=randint(1,n*n)
-    while x in placed:
-        x=randint(1,n*n)
-    placed.append(x)
-    state.beacons[b]=x
-i=1
-while i<=n**2:
-    state.clear[i]=1
-    if i<=n:
-        if i==1:
-            state.behind[i]=i+1
-        elif i==n:
-            state.infront[i]=i-1
+numBlocks=100
+towerSize=10
+chanceToStack=10
+stacked=[]
+for i in range(numBlocks):
+    state.clear[i]=True
+for i in range(numBlocks):
+    rand=randint(1,100)
+    block=None
+    if rand<chanceToStack:
+        block=randint(0,numBlocks-1)
+        if block==0:
+            start=99
         else:
-            state.behind[i]=i+1
-            state.infront[i]=i-1
-        state.above[i]=i+n
-    elif i>(n**2-n):
-        if i==(n**2-n+1):
-            state.behind[i]=i+1
-        elif i==n**2:
-            state.infront[i]=i-1
-        else:
-            state.behind[i]=i+1
-            state.infront[i]=i-1
-        state.below[i]=i-n
-    elif (i-1)%n==0:
-        state.below[i]=i-n
-        state.above[i]=i+n
-        state.behind[i]=i+1
-    elif i%n==0:
-        state.below[i]=i-n
-        state.above[i]=i+n
-        state.infront[i]=i-1
-    else:
-        state.below[i]=i-n
-        state.above[i]=i+n
-        state.infront[i]=i-1
-        state.behind[i]=i+1
-    i=i+1
+            start=block-1
+        while block in stacked or block==i:
+            if block==99:
+                block=0
+            elif block==start:
+                block=None
+                break
+            else:
+                block+=1
+    if i==block:
+        print('here')
+        time.sleep(5)
+    state.on[i]=block
+    if block!=None:
+        stacked.append(block)
+        state.clear[block]=False
 
-goals=[('light_all', 'Agent1', n)]
+goals=[('achieve_goal', towerSize)]
 treehop.declare_goals(goals)
 policy=treehop.pyhopT(state, goals,True)
-#treehop.print_policy(policy,state)
+treehop.print_policy(policy,state)
 
-gen_expectations(policy, state)
+#gen_expectations(policy, state)
 #treehop.print_plan_dfs(actions)
 #print_plan(Plan,exp='Rexp')
 
