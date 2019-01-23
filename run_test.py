c1354cfd6118fa6e665bfab422aae9e28084478a
Noah Reifsnyder <ndr217@lehigh.edu>
2019-01-23 08:30:18 -0500

    Generalizing run_test some more

--- run_test.py
+++ run_test.py
@@ -1,7 +1,6 @@
 # file to run environments for testing
-P = __import__('MWP')  # substitute problem file here
-D = __import__('MWD')  # substitue domain file here
-from MWD import *
+P = None
+D = None
 import pyhop
 from random import *
 from ExpectationsGenerator import *
@@ -12,10 +11,17 @@ import sys
 import time
 
 
+def set_domain(domain):
+    global P, D
+    p_string=domain+'P'
+    d_string=domain+'D'
+    P = __import__(p_string)
+    D = __import__(d_string)
+
 def determine_value(val_range):
-    fuel_range = val_range[1] - val_range[0]
+    ran_range = val_range[1] - val_range[0]
     ran_val = random()
-    ran_val *= fuel_range
+    ran_val *= ran_range
     ran_val += val_range[0]
     ran_val = round(ran_val, 2)
     print(val_range, ran_val)
@@ -27,8 +33,18 @@ def reload_pyhop():
     pyhop.verticies = []
 
 
-agent = 'Agent1'
 reqP = .5  # required percentage for expectations to be true
+
+
+def take_action(state):
+    prev = copy.deepcopy(state)
+    counter += 1
+    action = policy[state]
+    returnval = getattr(D, action.name[0])(state, *action.name[1:])
+    state = action.children[0]
+    for numeric_value in pyhop.numeric_values:
+        for key in getattr(state, numeric_value):
+            getattr(state, numeric_value)[key] = determine_value(getattr(state, numeric_value)[key])
 counter = 0  # keeps track of actions taken
 fuel_consumed = 0
 countFailed = 0  # keeps track of replans
@@ -36,9 +52,8 @@ num_runs = 10
 for x in range(0, num_runs):
     reload_pyhop()
     reload(P)
-    print(x)
     state = copy.deepcopy(P.state)  # gets starting state from problem file
-    policy = treehop.pyhop_t(state, treehop.goals, True)
+    policy = pyhop.pyhop_t(state, pyhop.goals, True)
     gen_expectations(policy, state)
     while state in policy:
         prev = copy.deepcopy(state)
