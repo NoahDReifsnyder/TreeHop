59e665166cdb1fcfc0cf687d3796283fe73d3f46
Noah Reifsnyder <ndr217@lehigh.edu>
2019-03-06 11:13:07 -0500

    starting testing, function change.

--- testD.py
+++ testD.py
@@ -8,62 +8,11 @@ err = .1
 ND = True  # Action type
 
 
-def stack(state, block1, block2):  # forward or up
-    state1 = copy.copy(state)
-    if state.clear[block1] and state.clear[block2] and state.energy[1][0] >= G_eff:
-        preconditions = {'clear': {block1: True, block2: True}, 'energy': {1: (10, 'inf')}}
-        state.on[block1] = block2
-        state.clear[block2] = False
-        state.energy[1] = (state.energy[1][0] - (G_eff + err), state.energy[1][1] - (G_eff - err))
-        if ND:
-            return [state, state1], preconditions,
-        else:
-            return [state], preconditions,
-    else:
-        print('stack')
-        print(state.clear[block1], state.clear[block2])
-        print(block1, block2)
-        return False
+def move(state):
+    precond = {}
+    return [state], precond
 
 
-treehop.declare_operators(stack)
-
-
-def find_tallest(state):
-    size = 0
-    top = None
-    for block in state.clear:
-        temp_size = 0
-        temp_top = block
-        while block in state.on:
-            temp_size += 1
-            block = state.on[block]
-        if temp_size > size:
-            size = temp_size
-            top = temp_top
-    return size, top
-
-        
-def achieve_goal(state, n):
-    size, top = find_tallest(state)
-    clear = copy.deepcopy(state.clear)
-    n = n-size
-    moves = []
-    used = []
-    energy = state.energy[1][0]
-    for i in range(n):
-        if energy < G_eff:
-            moves.append('recharge')
-        block = choice([block for block in clear if clear[block] and block != top and block not in used])
-        if not state.clear[block]:
-            print(str(block)+" IS NOT CLEAR")
-        moves.append(('stack', block, top))
-        clear[top] = False
-        energy -= (G_eff + err)
-        top = block
-        used.append(block)
-    print(moves)
-    return moves
-
-
-treehop.declare_methods('achieve_goal', achieve_goal)
+def achieve_goal(state):
+    actions = []
+    return actions
--- testP.py
+++ testP.py
@@ -6,42 +6,4 @@ import time
 
 
 state = treehop.State('state')
-state.on = {}
-state.clear = {}
-state.energy = {1: (100, 100)}
-numBlocks = 100
-towerSize = 10
-chanceToStack = 10
-stacked = []
-for i in range(numBlocks):
-    state.clear[i] = True
-for i in range(numBlocks):
-    rand = randint(1, 100)
-    block = None
-    if rand < chanceToStack:
-        block = randint(0, numBlocks-1)
-        if block == 0:
-            start = 99
-        else:
-            start = block-1
-        while block in stacked or block == i:
-            if block == 99:
-                block = 0
-            elif block == start:
-                block = None
-                break
-            else:
-                block += 1
-    if i == block:
-        print('here')
-        time.sleep(5)
-    state.on[i] = block
-    if block is not None:
-        stacked.append(block)
-        state.clear[block] = False
-
-goals = [('achieve_goal', towerSize)]
-treehop.declare_goals(goals)
-policy = treehop.pyhop_t(state, goals, True)
-treehop.print_policy(policy, state)
-gen_expectations(policy, state)
+state.fuel = {'Agent1' : [10, 10]}
