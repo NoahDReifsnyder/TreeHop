3379b7cc8ce595e75e53cb6d6f08d1d22059208f
NoahDReifsnyder <noahreifsnyder@gmail.com>
2019-04-15 14:33:14 -0400

    Lambda functions

--- RD.py
+++ RD.py
@@ -1,17 +1,15 @@
 import pyhop as treehop
-import numpy
-
-def solve_equation(eq, time):
-    eq = eq.replace("t", str(time))
-    y = eval(eq)
-    return y
+import numpy as np
 
 
-def set_fov(state, cam, left, right, start, end):
+
+def set_l_fov(state, cam, theta):
+    state.l_fov[cam] = theta
     return
 
 
-def set_angle(state, cam, start, end):
+def set_r_fov(state, cam, theta):
+    state.r_fov[cam] = theta
     return
 
 
@@ -19,21 +17,55 @@ def wait(state):
     return
 
 
-treehop.declare_operators(set_fov, set_angle)
+treehop.declare_operators(set_l_fov, set_r_fov, wait)
 
 
-def achieve_goal(state):
+def achieve_goal(state, cam):
     left, right = furthest(state)
-    left_s, right_s = largest_slopes(state)
+    # left_s, right_s = largest_slopes(state)
     plan = []
     print(left, right)
-    print(left_s, right_s)
-    angle_l = solve_equation(state.actors_t[left[1]], state.time['time'])
-    angle_r = solve_equation(state.actors_t[right[1]], state.time['time'])
-    angle_l_s = state.actors_t[left[0]]
-    angle_r_s = state.actors_t[right[0]]
-    print(angle_l, angle_r,)
-    numpy.arc
+    l_y = state.actors_y[left[1]]
+    l_x = state.actors_x[left[1]]
+
+    def l_t(t):
+        return np.arctan2(l_y(t), l_x(t))
+    r_y = state.actors_y[right[1]]
+    r_x = state.actors_x[right[1]]
+
+    def r_t(t):
+        return np.arctan2(r_y(t), r_x(t))
+    time = state.time['time']
+    if not state.l_fov[cam] == l_t:
+        plan.append(('set_l_fov', cam, l_t))
+    if not state.r_fov[cam] == r_t:
+        plan.append(('set_r_fov', cam, r_t))
+    wait_time = 0
+    for t in range(0, 100):
+        if wait_time > 0:
+            break
+        for actor in state.actors_x:
+            if not actor == left[1]:
+                l_y_new = state.actors_y[actor]
+                l_x_new = state.actors_x[actor]
+                t = l_t(t)
+                new_t = np.arctan2(l_y_new(t), l_x_new(t))
+                if new_t < t:
+                    print(t, new_t)
+                    wait_time = t
+                    break
+            if not actor == right[1]:
+                r_y_new = state.actors_y[actor]
+                r_x_new = state.actors_x[actor]
+                t = r_t(t)
+                new_t = np.arctan2(r_y_new(t), r_x_new(t))
+                if new_t > t:
+                    print(t, new_t)
+                    wait_time = t
+                    break
+    if wait_time>0:
+        plan.append(("wait", t))
+        plan.append(("achieve_goal", cam))
     return plan
 
 
@@ -55,8 +87,13 @@ def furthest(state):
     left = (None, -1)
     right = (None, -1)
     t = state.time['time']
-    for actor in state.actors_t:
-        loc_t = solve_equation(state.actors_t[actor], t)
+    for actor in state.actors_x:
+        print(actor)
+        print(state.actors_x[actor](t))
+        loc_x = state.actors_x[actor](t)
+        loc_y = state.actors_y[actor](t)
+        loc_t = np.arctan2(loc_y, loc_x)
+        print(loc_x, loc_y, loc_t)
         if actor != left[1]:
             if left[0] is None or loc_t < left[0]:
                 left = (loc_t, actor)
--- RP.py
+++ RP.py
@@ -1,23 +1,31 @@
 from RD import *
 from random import *
-from ExpectationsGenerator import *
-import time
+import pyhop as treehop
 
-starting_fov = 90
-starting_angle = 0
+starting_l_fov = 0
+starting_r_fov = 0
 state = treehop.State('state')
 state.boundaries = {}
-state.fov = {'cam1': starting_fov}  # at 0,0 in the coordinate system
-state.angle = {'cam1': starting_angle}
-state.actors_t = {}
-state.actors_r = {}
+state.l_fov = {'cam1': starting_l_fov}  # at 0,0 in the coordinate system
+state.r_fov = {'cam1': starting_r_fov}
+state.actors_x = {}
+state.actors_y = {}
 num_actors = 3
 state.time = {'time': 0}
 for i in range(0, num_actors):
-    slope = uniform(-1, 1)
-    eq = str(slope) + "*t+" + str(i+1)
-    solve_equation(eq, 1)
-    state.actors_r[i] = eq
-print(state.actors_t)
-plan = achieve_goal(state)
-print(plan)
+    slope_1 = uniform(-1, 1)
+
+    def f(t, temp=i):
+        return slope_1 * t + temp + 1
+    state.actors_x[i] = f
+    slope_2 = uniform(0, 1)
+
+    def g(t, temp=i):
+        return slope_2 * t + temp + 1
+    state.actors_y[i] = g
+print(state.actors_y[1], state.actors_y[2])
+plan = achieve_goal(state, 'cam1')
+goals = [('achieve_goal', 'cam1')]
+treehop.declare_goals(goals)
+policy = treehop.pyhop_t(state, original_call=True)
+print(policy)
